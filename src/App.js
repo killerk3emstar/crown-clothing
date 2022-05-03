1cd6993de5cd256ea1e58a038746b66d667d43be
killerkeemstar <gguess1337@gmail.com>
2022-05-03 15:57:03 +0200

    added routing adding firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import "./categories.styles.scss";
-import { categories } from "./categories";
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component.jsx";
+import { Routes, Route } from "react-router-dom";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1> Da Szop</h1>
+    </div>
+  );
+};
 
 const App = () => {
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
