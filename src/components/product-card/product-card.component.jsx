2d53a7d1a23931175cdaeb712e5b9e5c7c21bb05
killerkeemstar <gguess1337@gmail.com>
2022-07-13 15:09:05 +0200

    finish cart dropdown

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,15 +1,25 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addItemToCartHandler = () => {
+    addItemToCart(product);
+  };
+
   return (
     <div className="product-card-container">
       <img alt={`$name`} src={imageUrl} />
       <div className="footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
-        <Button buttonType="inverted">Add to cart</Button>
+        <Button buttonType="inverted" onClick={addItemToCartHandler}>
+          Add to cart
+        </Button>
       </div>
     </div>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -11,7 +11,7 @@ import "./navigation.styles.scss";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
